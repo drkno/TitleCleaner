e533e1ed19553a37f17c208827edbb27ed0e7f2f
Matthew <mrk45@uclive.ac.nz>
2014-11-23 21:02:36 +1300

    Version number format change

--- TitleCleanerGui/Properties/AssemblyInfo.cs
+++ TitleCleanerGui/Properties/AssemblyInfo.cs
@@ -40,6 +40,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.0.0.1")]
-[assembly: AssemblyFileVersion("1.0.0.1")]
+[assembly: AssemblyVersion("1.0.1.14327")]
+[assembly: AssemblyFileVersion("1.0.1.14327")]
 [assembly: NeutralResourcesLanguage("en-NZ")]
