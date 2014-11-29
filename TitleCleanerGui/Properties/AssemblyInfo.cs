6a4cc0f3c26904477b090fc55d1c09396e4594bc
Matthew <mrk45@uclive.ac.nz>
2014-11-29 22:07:18 +1300

    Actually include

--- TitleCleanerGui/Properties/AssemblyInfo.cs
+++ TitleCleanerGui/Properties/AssemblyInfo.cs
@@ -40,6 +40,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.0.5.14333")]
-[assembly: AssemblyFileVersion("1.0.5.14333")]
+[assembly: AssemblyVersion("1.0.6.14333")]
+[assembly: AssemblyFileVersion("1.0.6.14333")]
 [assembly: NeutralResourcesLanguage("en-NZ")]
