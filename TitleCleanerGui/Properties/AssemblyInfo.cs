c39a07ff36cd84edc4755ea25d75c07caa7f7f75
Matthew <mrk45@uclive.ac.nz>
2014-11-29 19:25:06 +1300

    MediaQuality performance enhancements

--- TitleCleanerGui/Properties/AssemblyInfo.cs
+++ TitleCleanerGui/Properties/AssemblyInfo.cs
@@ -40,6 +40,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.0.3.14331")]
-[assembly: AssemblyFileVersion("1.0.3.14331")]
+[assembly: AssemblyVersion("1.0.5.14333")]
+[assembly: AssemblyFileVersion("1.0.5.14333")]
 [assembly: NeutralResourcesLanguage("en-NZ")]
