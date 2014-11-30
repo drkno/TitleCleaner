4d13d288f18aa1c0b5983b829a0af375bee9a9f1
Matthew <mrk45@uclive.ac.nz>
2014-11-30 18:34:07 +1300

    Add overview

--- TitleCleanerGui/Properties/AssemblyInfo.cs
+++ TitleCleanerGui/Properties/AssemblyInfo.cs
@@ -40,6 +40,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.0.7.14334")]
-[assembly: AssemblyFileVersion("1.0.7.14334")]
+[assembly: AssemblyVersion("1.0.9.14334")]
+[assembly: AssemblyFileVersion("1.0.9.14334")]
 [assembly: NeutralResourcesLanguage("en-NZ")]
